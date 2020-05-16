cf5ad83c94bf0ac93159bc4cdf4a94a31bb29bc8
Your Name <alejandro.saenz@live.com>
2020-05-16 15:24:03 -0400

    updated server and routes alejandro

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
-
 const express = require('express');
 const path = require('path');
 const app = express();
+const weatherRoutes = require('./routes/weather');
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use('/weather', weatherRoutes);
   // Handle React routing, return all requests to React app
   app.get('*', (request, response) => {
     response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
@@ -27,3 +28,5 @@ const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`API listening on port ${port}...`);
 });
+
+
