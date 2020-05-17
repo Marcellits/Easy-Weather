2bf2e74d9b996288d73bb86775564542e164ab6c
Leo <lpolicastro@pm.me>
2020-05-17 12:09:59 -0400

    push to show laz

--- client/src/components/City.jsx
+++ client/src/components/City.jsx
@@ -1,29 +1,30 @@
-import React, {useState, useEffect} from 'react'
-import {useParams} from 'react-router-dom'
-import Weather5days from './Weather5days'
-import GenMessage from './GenMessage'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Weather5days from './Weather5days';
+import GenMessage from './GenMessage';
 
 const City = (props) => {
-    const [cityData, setCityData] = useState(null)
-    const {city} = props.match.params
+  const [cityData, setCityData] = useState(null);
+  const { city } = props.match.params;
 
-    useEffect(() => {
-        fetch(`/city/${city}`)
-        .then(response => response.json())
-        .then(data => setCityData(data.current))
-    }, [])
+  useEffect(() => {
+    fetch(`/city/${city}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCityData(data.current);
+      });
+  }, []);
 
-    console.log(cityData)
-    return (
-        <div>
-            <h1>This is the weather results page</h1>
-            <h1>Current temperature for {city} is {cityData && cityData.temp_f} °F</h1>
-        <GenMessage />
-        <Weather5days/>
-        
-        </div>
+  return (
+    <div>
+      <h1>This is the weather results page</h1>
+      <h1>
+        Current temperature for {city} is {cityData && cityData.temp_f} °F
+      </h1>
+      <GenMessage info={cityData} />
+      <Weather5days />
+    </div>
+  );
+};
 
-    )
-}
-
-export default City
+export default City;
--- client/src/components/GenMessage.jsx
+++ client/src/components/GenMessage.jsx
@@ -1,35 +1,39 @@
 import React, { useEffect, useState } from 'react';
-import {Container} from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import axios from 'axios';
 
-const GenMessage = () => {
-    const [arrayMessage, setArrayMessage] = useState([ "Today is going to be cold, take a coat.", 
-    "Get your sunglasses and the sunscreen! Today is a good day to go to the beach.",
-    "When leaving the house, it may be a good idea to take the umbrella.",
-    ])
+const GenMessage = (props) => {
+  const [arrayMessage, setArrayMessage] = useState([
+    'Today is going to be cold, take a coat.',
+    'Get your sunglasses and the sunscreen! Today is a good day to go to the beach.',
+    'When leaving the house, it may be a good idea to take the umbrella.'
+  ]);
 
-
-
-   
-    //I have to connect the condition data to the array
-    //and print this value 
-    const weatherCond = 'snowing'
-    const Message =() => {
-        if (weatherCond === 'Sunny day'){
-            console.log(arrayMessage[1])
-        }else if (weatherCond === 'raining day' ){
-            console.log (arrayMessage[2])
-        } else if (weatherCond === 'snowing'){
-            console.log (arrayMessage[0])
-        }
+  //I have to connect the condition data to the array
+  //and print this value
+  const weatherCond = 'snowing';
+  const message = () => {
+    if (weatherCond === 'Sunny day') {
+      console.log(arrayMessage[1]);
+    } else if (weatherCond === 'raining day') {
+      console.log(arrayMessage[2]);
+    } else if (weatherCond === 'snowing') {
+      console.log(arrayMessage[0]);
     }
-    Message();
+  };
 
-    return(
-        <Container>
-       
-    <h2>The message goes here! {arrayMessage[0]} </h2>  
-       </Container>
-    )
-}
-export default GenMessage    
+  // useEffect(message(), []);
+
+  return (
+    <Container>
+      {props.info ? (
+        <h1>{props.info.condition.text}</h1>
+      ) : (
+        <h1>Nothing yet!</h1>
+      )}
+
+      <h2>The message goes here! {arrayMessage[0]} </h2>
+    </Container>
+  );
+};
+export default GenMessage;
